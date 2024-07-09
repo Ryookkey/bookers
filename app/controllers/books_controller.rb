a81de6404c1e88cb775b4103b45d32830d6b5a17
Ryookkey <173231919+Ryookkey@users.noreply.github.com>
2024-07-09 13:35:11 +0000

    2nd commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,15 +1,16 @@
 class BooksController < ApplicationController
   def index
-    @book = Book.new
+    @book = Book.all
   end
 
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to '/show'
+    # redirect_to '/books/:id'
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
